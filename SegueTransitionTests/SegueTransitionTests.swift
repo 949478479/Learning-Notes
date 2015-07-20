48f4b54397789610209ce7e9dc334f661ad02033
949478479 <949478479@qq.com>
2015-07-20 16:15:01 +0800

    Initial Commit

---
+++ SegueTransitionTests/SegueTransitionTests.swift
@@ -0,0 +1,36 @@
+//
+//  SegueTransitionTests.swift
+//  SegueTransitionTests
+//
+//  Created by 从今以后 on 15/7/20.
+//  Copyright (c) 2015年 949478479. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SegueTransitionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
