9cdcad40f5ed6071d68c4ade41bffdce17eb0a27
949478479 <949478479@qq.com>
2015-07-24 02:17:46 +0800

    ...

--- CardAnimation/CardView.swift
+++ CardAnimation/CardView.swift
@@ -26,13 +26,13 @@ class CardView: UIView {
     var maxCountOfCardsForDisplay = 5
     var maxRotation               = CGFloat(M_PI_2 / 6)
 
+    private var lastFrame    = CGRectNull
     private var isBackside   = false // 表示最前面的卡片是否被翻转.
-    private var animating    = false
     private var currentIndex = 0 // 最前面的卡片的图片索引.
     private var totalOfCards = 0
 
     private var visibleCards = [UIImageView]()
-    private lazy var reuseCard: UIImageView! = self.p_createCard()
+    private lazy var reuseCard: UIImageView! = self.createCard()
 
     private var maxOffset: CGFloat { return bounds.width / 2 }
     private var totalOfSurplusCards: Int { return totalOfCards - currentIndex }
@@ -42,34 +42,53 @@ class CardView: UIView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        p_createCards()
+        createCards()
     }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        p_createCards()
+        createCards()
     }
 
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
 
+    // MARK: - 布局
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layoutCards()
+    }
+
+    private func layoutCards() {
+        if lastFrame != frame { // 动画中会频繁调用此方法,限制一下.
+            lastFrame = frame
+            for (index, card) in enumerate(visibleCards) {
+                let layerTransform   = card.layer.transform
+                card.layer.transform = CATransform3DIdentity
+                card.frame           = CGRectInset(bounds, 20, 100)
+                card.layer.transform = layerTransform
+            }
+        }
+    }
+
     // MARK: - 创建卡片
 
-    private func p_createCards() {
+    private func createCards() {
         totalOfCards = delegate!.numberOfCardsInCardView(self)
         let count = maxCountOfCardsForDisplay < totalOfCards ? maxCountOfCardsForDisplay : totalOfCards
         for index in 0..<count {
-            let card = p_createCard()
+            let card = createCard()
             card.image = delegate?.cardView(self, imageForCardAtIndex: index)
-            p_configureCard(card, atIndex: index)
-            p_configureCardTransform(card, atIndex: index)
+            configureCard(card, atIndex: index)
+            configureCardTransform(card, atIndex: index)
             visibleCards.append(card)
             insertSubview(card, atIndex: 0)
         }
     }
 
-    private func p_createCard() -> UIImageView {
+    private func createCard() -> UIImageView {
         let card = UIImageView(frame: CGRectInset(bounds, 20, 100))
 
         card.userInteractionEnabled = true
@@ -84,18 +103,18 @@ class CardView: UIView {
 
     // MARK: - 配置卡片
 
-    private func p_configureCardTransform(card: UIImageView, atIndex index: Int) {
+    private func configureCardTransform(card: UIImageView, atIndex index: Int) {
         // 随着索引变换,每次缩放变化 10%, 向上平移 8 点距离.
         card.layer.setValue(1 - 0.1 * CGFloat(index), forKeyPath: "transform.scale.x")
         card.layer.setValue(-8 * CGFloat(index), forKeyPath: "transform.translation.y")
     }
 
-    private func p_configureCard(card: UIImageView, atIndex index: Int) {
+    private func configureCard(card: UIImageView, atIndex index: Int) {
         if index == 0 {
             card.layer.zPosition = UIScreen.mainScreen().bounds.width // 确保最前面的卡片有足够的空间旋转.
 
-            card.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "p_tapGestureHandle:"))
-            card.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: "p_panGestureHandle:"))
+            card.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "tapGestureHandle:"))
+            card.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: "panGestureHandle:"))
         } else {
             card.layer.zPosition = 0
             
@@ -105,56 +124,32 @@ class CardView: UIView {
         }
     }
 
-    // MARK: - 布局
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        p_layoutCards()
-    }
-
-    private func p_layoutCards() {
-        if animating { return } // 动画中会频繁调用此方法,限制一下.
-        for (index, card) in enumerate(visibleCards) {
-            let layerTransform   = card.layer.transform
-            card.layer.transform = CATransform3DIdentity
-            card.frame           = CGRectInset(bounds, 20, 100)
-            card.layer.transform = layerTransform
-        }
-    }
-
     // MARK: - 手势识别
 
-    @objc private func p_tapGestureHandle(tap: UITapGestureRecognizer) {
+    @objc private func tapGestureHandle(tap: UITapGestureRecognizer) {
         isBackside = !isBackside
-        animating  = true
-        UIView.animateWithDuration(2 * animationDuration, animations: {
+        UIView.animateWithDuration(2 * animationDuration) {
             tap.view!.layer.setValue(CGFloat(M_PI), forKeyPath: "transform.rotation.y")
-        }, completion: { _ in
-            self.animating = false
-        })
+        }
     }
     
-    @objc private func p_panGestureHandle(pan: UIPanGestureRecognizer) {
+    @objc private func panGestureHandle(pan: UIPanGestureRecognizer) {
         let offset = pan.translationInView(self).x
 
         switch pan.state {
         case .Changed:
             if abs(offset) > maxOffset {
-                p_throwCardWithAnimation()
-                p_showNewCardWithAnimation()
+                throwCardWithAnimation()
+                showNewCardWithAnimation()
                 pan.enabled = false // 超过最大幅度后立即终止手势交互,会立即触发 .Cancelled .
             } else {
-                p_transformCardsForOffset(offset)
+                transformCardsForOffset(offset)
             }
-
-        case .Began:
-            animating = true // 为了拖动时不要触发 layoutSubviews.
-
         case .Ended, .Cancelled:
             if !pan.enabled {
                 pan.enabled = true
             } else {
-                p_resetCardsWithAnimation()
+                resetCardsWithAnimation()
             }
 
         default: break
@@ -163,11 +158,11 @@ class CardView: UIView {
 
     // MARK: - 重用卡片
 
-    private func p_reuseCard(card: UIImageView) {
+    private func reuseCard(card: UIImageView) {
         if totalOfSurplusCards < maxCountOfCardsForDisplay { return }
 
-        p_configureCard(reuseCard, atIndex: visibleCards.count)
-        p_configureCardTransform(reuseCard, atIndex: visibleCards.count)
+        configureCard(reuseCard, atIndex: visibleCards.count)
+        configureCardTransform(reuseCard, atIndex: visibleCards.count)
         reuseCard.image = delegate?.cardView(self, imageForCardAtIndex: currentIndex + maxCountOfCardsForDisplay - 1)
 
         visibleCards.append(reuseCard)
@@ -178,15 +173,13 @@ class CardView: UIView {
 
     // MARK: - 动画
 
-    private func p_resetCardsWithAnimation() {
+    private func resetCardsWithAnimation() {
         UIView.animateWithDuration(animationDuration, delay: 0, usingSpringWithDamping: 0.1, initialSpringVelocity: 0, options: nil, animations: {
-            self.p_transformCardsForOffset(0)
-        }, completion: { _ in
-            self.animating = false
-        })
+            self.transformCardsForOffset(0)
+        }, completion: nil)
     }
 
-    private func p_throwCardWithAnimation() {
+    private func throwCardWithAnimation() {
         ++currentIndex
 
         var shouldReset = false
@@ -199,7 +192,7 @@ class CardView: UIView {
         let initalTranslationX = firstCard.layer.valueForKeyPath("transform.translation.x") as! CGFloat
         let finalTranslationX  = ((initalTranslationX > 0) ? 1 : -1) * (bounds.width + bounds.height * sin(maxRotation))
 
-        p_reuseCard(firstCard)
+        reuseCard(firstCard)
 
         UIView.animateWithDuration(animationDuration, animations: {
             firstCard.layer.setValue(finalTranslationX, forKeyPath: "transform.translation.x")
@@ -212,24 +205,22 @@ class CardView: UIView {
         })
     }
 
-    private func p_showNewCardWithAnimation() {
+    private func showNewCardWithAnimation() {
         for (index, card) in enumerate(self.visibleCards) {
-            p_configureCard(card, atIndex: index)
-            p_transformCard(card, forOffset: maxOffset, atIndex: index) // 调整偏移至最大,让动画更明显.
+            configureCard(card, atIndex: index)
+            transformCard(card, forOffset: maxOffset, atIndex: index) // 调整偏移至最大,让动画更明显.
         }
         UIView.animateWithDuration(animationDuration, delay: 0, usingSpringWithDamping: 0.1, initialSpringVelocity: 0, options: nil, animations: {
             for (index, card) in enumerate(self.visibleCards) {
-                self.p_configureCardTransform(card, atIndex: index)
-                self.p_transformCard(card, forOffset: 0, atIndex: index)
+                self.configureCardTransform(card, atIndex: index)
+                self.transformCard(card, forOffset: 0, atIndex: index)
             }
-        }, completion: { _ in
-            self.animating = false
-        })
+        }, completion: nil)
     }
 
     // MARK: - 变换卡片
 
-    private func p_transformCard(card: UIImageView, forOffset offset: CGFloat, atIndex index: Int) {
+    private func transformCard(card: UIImageView, forOffset offset: CGFloat, atIndex index: Int) {
         // 使 card 的平移量和索引挂钩,越靠后平移量越小.
         let translationX = (offset != 0) ? offset * (1 - CGFloat(index) / CGFloat(visibleCards.count)) : 0
         card.layer.setValue(translationX, forKeyPath: "transform.translation.x")
@@ -240,9 +231,9 @@ class CardView: UIView {
         card.layer.setValue(rotationZ, forKeyPath: "transform.rotation.z")
     }
 
-    private func p_transformCardsForOffset(offset: CGFloat ) {
+    private func transformCardsForOffset(offset: CGFloat ) {
         for (index, card) in enumerate(self.visibleCards) {
-            p_transformCard(card, forOffset: offset, atIndex: index)
+            transformCard(card, forOffset: offset, atIndex: index)
         }
     }
 }
