94e1caab18d73e4e2f88ff221a459398cd35bf4f
949478479 <949478479@qq.com>
2015-07-25 17:52:50 +0800

    整理一下.

--- FlipTransion/DismissAnimationController.swift
+++ FlipTransion/DismissAnimationController.swift
@@ -8,51 +8,56 @@
 
 import UIKit
 
-private let kTransitionDuration: NSTimeInterval = 1
-
 class DismissAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
 
+    let transitionDuration: NSTimeInterval = 1
+
     func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
-        return kTransitionDuration
+        return transitionDuration
     }
 
     func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
 
-        let presentingVC = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey) as! PresentingViewController
-        let presentedVC  = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey) as! PresentedViewController
+        let containerView = transitionContext.containerView()
 
-        let finalFrame = transitionContext.finalFrameForViewController(presentingVC)
+        let presentingVC  = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+        let presentedVC   = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
 
-        let coverView: UIView = {
-            let cover                 = UIView(frame: presentingVC.view.bounds)
-            cover.alpha               = 0.8
-            cover.backgroundColor     = UIColor.blackColor()
-            cover.layer.anchorPoint.x = 0
-            cover.layer.position.x    = finalFrame.minX
-            cover.layer.transform     = CATransform3DMakeRotation(CGFloat(-M_PI_2), 0, 1, 0)
-            return cover
-        }()
-        
+        let finalFrame    = transitionContext.finalFrameForViewController(presentingVC)
+
+        let coverView     = UIView(frame: finalFrame)
+
+        coverView.alpha               = 0.8
+        coverView.backgroundColor     = UIColor.blackColor()
+        coverView.layer.anchorPoint.x = 0
+        coverView.layer.position.x    = finalFrame.minX
+        coverView.layer.transform     = CATransform3DMakeRotation(CGFloat(-M_PI_2), 0, 1, 0)
+
         presentingVC.view.layer.shouldRasterize    = true
         presentingVC.view.layer.rasterizationScale = UIScreen.mainScreen().scale
         presentingVC.view.layer.anchorPoint.x      = 0
         presentingVC.view.layer.position.x         = finalFrame.minX
         presentingVC.view.layer.transform          = CATransform3DMakeRotation(CGFloat(-M_PI_2), 0, 1, 0)
 
-        let containerView = transitionContext.containerView()
         containerView.layer.sublayerTransform.m34 = -1 / 1_000
         containerView.addSubview(presentingVC.view)
         containerView.addSubview(coverView)
 
-        UIView.animateWithDuration(kTransitionDuration, delay: 0, options: .CurveLinear, animations: {
+        UIView.animateWithDuration(transitionDuration, delay: 0, options: .CurveLinear, animations: {
+
             coverView.alpha                   = 0
             coverView.layer.transform         = CATransform3DIdentity
+
             presentingVC.view.layer.transform = CATransform3DIdentity
+
         }, completion: { _ in
+
             coverView.removeFromSuperview()
+
             presentingVC.view.layer.shouldRasterize = false
             presentingVC.view.layer.anchorPoint.x   = 0.5
             presentingVC.view.layer.position.x      = finalFrame.midX
+
             transitionContext.completeTransition(!transitionContext.transitionWasCancelled())
         })
     }
--- FlipTransion/PresentAnimationController.swift
+++ FlipTransion/PresentAnimationController.swift
@@ -8,30 +8,29 @@
 
 import UIKit
 
-private let kTransitionDuration: NSTimeInterval = 1
-
 class PresentAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
 
+    let transitionDuration: NSTimeInterval = 1
+
     func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
-        return kTransitionDuration
+        return transitionDuration
     }
 
     func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
 
-        let presentedVC = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey) as! PresentedViewController
-        let presentingVC = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey) as! PresentingViewController
+        let containerView = transitionContext.containerView()
 
-        let initalFrame = transitionContext.initialFrameForViewController(presentingVC)
+        let presentedVC   = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+        let presentingVC  = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
 
-        // 添加遮罩,实现明暗变化.
-        let coverView: UIView = {
-            let cover                 = UIView(frame: initalFrame)
-            cover.alpha               = 0
-            cover.backgroundColor     = UIColor.blackColor()
-            cover.layer.anchorPoint.x = 0
-            cover.layer.position.x    = initalFrame.minX
-            return cover
-        }()
+        let initalFrame   = transitionContext.initialFrameForViewController(presentingVC)
+
+        let coverView     = UIView(frame: initalFrame) // 添加遮罩,实现明暗变化.
+
+        coverView.alpha               = 0
+        coverView.backgroundColor     = UIColor.blackColor()
+        coverView.layer.anchorPoint.x = 0
+        coverView.layer.position.x    = initalFrame.minX
 
         // 改变锚点,使之以左边界为轴旋转.调整 position 使视图 frame 不变.开启光栅化,不然有毛边.
         presentingVC.view.layer.anchorPoint.x      = 0
@@ -39,22 +38,27 @@ class PresentAnimationController: NSObject, UIViewControllerAnimatedTransitionin
         presentingVC.view.layer.shouldRasterize    = true
         presentingVC.view.layer.rasterizationScale = UIScreen.mainScreen().scale
 
-        let containerView = transitionContext.containerView()
         containerView.layer.sublayerTransform.m34 = -1 / 1_000 // 改变 m34, 获得立体透视效果.
         containerView.insertSubview(presentedVC.view, belowSubview: presentingVC.view)
         containerView.addSubview(coverView)
 
         // 将视图以左边界为轴向屏幕外旋转90°,动画结束后恢复锚点,移除遮罩.注意恢复仿射变换,否则frame受影响.
-        UIView.animateWithDuration(kTransitionDuration, delay: 0, options: .CurveLinear, animations: {
+        UIView.animateWithDuration(transitionDuration, delay: 0, options: .CurveLinear, animations: {
+
             coverView.alpha                   = 0.8
             coverView.layer.transform         = CATransform3DMakeRotation(CGFloat(-M_PI_2), 0, 1, 0)
+
             presentingVC.view.layer.transform = CATransform3DMakeRotation(CGFloat(-M_PI_2), 0, 1, 0)
+
         }, completion: { _ in
+
             coverView.removeFromSuperview()
+
             presentingVC.view.layer.shouldRasterize = false
             presentingVC.view.layer.anchorPoint.x   = 0.5
             presentingVC.view.layer.position.x      = initalFrame.midX
             presentingVC.view.layer.transform       = CATransform3DIdentity
+
             transitionContext.completeTransition(!transitionContext.transitionWasCancelled())
         })
     }
