a9046657e4846bdc2eb6fedd43832715151fe2cc
949478479 <949478479@qq.com>
2015-07-26 19:31:43 +0800

    Initial Commit

---
+++ SimpleWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleWeather
+//
+//  Created by 从今以后 on 15/7/26.
+//  Copyright (c) 2015年 949478479. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleWeather
+//
+//  Created by 从今以后 on 15/7/26.
+//  Copyright (c) 2015年 949478479. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
