9a7f5b244c9db4bdb5ad95d3a6fdcc439919f2dc
949478479 <949478479@qq.com>
2015-08-01 11:03:10 +0800

    Initial Commit

---
+++ FlickrSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlickrSearch
+//
+//  Created by 从今以后 on 15/8/1.
+//  Copyright (c) 2015年 从今以后. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FlickrSearch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlickrSearch
+//
+//  Created by 从今以后 on 15/8/1.
+//  Copyright (c) 2015年 从今以后. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
