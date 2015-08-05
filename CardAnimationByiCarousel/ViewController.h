adaafeab0ffdc7904e986ce3e90e2535e1cb42dd
949478479 <949478479@qq.com>
2015-08-05 23:41:58 +0800

    Initial Commit

---
+++ CardAnimationByiCarousel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardAnimationByiCarousel
+//
+//  Created by 从今以后 on 15/8/5.
+//  Copyright (c) 2015年 从今以后. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CardAnimationByiCarousel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CardAnimationByiCarousel
+//
+//  Created by 从今以后 on 15/8/5.
+//  Copyright (c) 2015年 从今以后. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
