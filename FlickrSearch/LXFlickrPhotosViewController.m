a1fe90611991544d0b0f78e94988a0995f7564a4
949478479 <949478479@qq.com>
2015-08-05 23:35:08 +0800

    先提交一下...

--- FlickrSearch/LXFlickrPhotosViewController.m
+++ FlickrSearch/LXFlickrPhotosViewController.m
@@ -6,12 +6,14 @@
 //  Copyright (c) 2015年 从今以后. All rights reserved.
 //
 
-#import <ObjectiveFlickr.h>
+#import <MBProgressHUD.h>
 
 #import "LXFlickr.h"
 #import "LXFlickrPhoto.h"
+#import "MBProgressHUD+LX.h"
 #import "LXFlickrPhotoCell.h"
 #import "LXFlickrPhotoHeaderView.h"
+#import "LXFlickrPhotoViewController.h"
 #import "LXFlickrPhotosViewController.h"
 
 static const CGFloat kPadding = 16;
@@ -22,13 +24,16 @@ static NSString * const kHeaderViewIdentifier = @"FlickrPhotoHeaderView";
 static NSString * const kOFSampleAppAPIKey          = @"77766361c226eb1cf362b9dc46d70c47";
 static NSString * const kOFSampleAppAPISharedSecret = @"44dce4451bb55ea1";
 
-@interface LXFlickrPhotosViewController () <UICollectionViewDelegateFlowLayout, UITextFieldDelegate, OFFlickrAPIRequestDelegate>
+@interface LXFlickrPhotosViewController () <UICollectionViewDelegateFlowLayout, UITextFieldDelegate, UIViewControllerTransitioningDelegate>
 
-@property (nonatomic, weak) IBOutlet UITextField    *textField;
-@property (nonatomic, weak) UIActivityIndicatorView *activityIndicator;
+@property (nonatomic, strong) MBProgressHUD        *hud;
+@property (nonatomic, weak  ) IBOutlet UITextField *textField;
 
+@property (nonatomic, strong) NSMutableArray      *photos;
 @property (nonatomic, strong) NSMutableArray      *searches;
 @property (nonatomic, strong) NSMutableDictionary *searchResults;
+@property (nonatomic, strong) NSIndexPath         *currentLargePhotoIndexPath;
+@property (nonatomic, strong) NSIndexPath         *previousLargePhotoIndexPath;
 
 @property (nonatomic, strong) LXFlickr *flickr;
 
@@ -43,49 +48,66 @@ static NSString * const kOFSampleAppAPISharedSecret = @"44dce4451bb55ea1";
     [super viewDidLoad];
 
     self.flickr        = [LXFlickr new];
+    self.photos        = [NSMutableArray new];
     self.searches      = [NSMutableArray new];
     self.searchResults = [NSMutableDictionary new];
 
     self.collectionView.backgroundColor =
         [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_cork"]];
+}
 
-    [self.textField addSubview:({
-        UIActivityIndicatorView *activityIndicator =
-            [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
-        activityIndicator.center = (CGPoint){
-            CGRectGetMidX(self.textField.bounds), CGRectGetMidY(self.textField.bounds)
-        };
-        self.activityIndicator = activityIndicator;
-        activityIndicator;
-    })];
+#pragma mark - HUD
+
+- (MBProgressHUD *)hud
+{
+    if (!_hud) {
+        _hud = [[MBProgressHUD alloc] initWithView:self.view];
+        _hud.dimBackground = YES;
+        [self.view.window addSubview:_hud];
+    }
+    return _hud;
 }
 
 #pragma mark - UITextFieldDelegate
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
-    textField.enabled = NO;
-
     [self.flickr flickrPhotosWithSearchString:textField.text
                                    completion:
      ^(NSString *searchString, NSArray *flickrPhotos, NSError *error) {
          
          if (flickrPhotos.count > 0 && ![self.searches containsObject:searchString]) {
+
              [self.searches insertObject:searchString atIndex:0];
              self.searchResults[searchString] = flickrPhotos;
 
              [self.collectionView reloadData];
          }
 
-         textField.enabled = YES;
-         [self.activityIndicator stopAnimating];
+         [self.hud hide:NO];
+         if (error) {
+             [MBProgressHUD lx_showHudForError:@"网络不给力啊..."];
+         }
      }];
 
-    [self.activityIndicator startAnimating];
+    [self.hud show:YES];
     [textField resignFirstResponder];
     textField.text = nil;
 
     return YES;
+}
+
+#pragma mark - UICollectionViewDelegateFlowLayout
+
+- (CGSize)collectionView:(UICollectionView *)collectionView
+                  layout:(UICollectionViewLayout *)collectionViewLayout
+  sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    LXFlickrPhoto *flickrPhoto = [self p_flickrPhotoForIndexPath:indexPath];
+
+    return (CGSize) {
+        flickrPhoto.thumbnailSize.width + kPadding, flickrPhoto.thumbnailSize.height + kPadding
+    };
 }
 
 #pragma mark - UICollectionViewDataSource
@@ -108,9 +130,7 @@ static NSString * const kOFSampleAppAPISharedSecret = @"44dce4451bb55ea1";
         [collectionView dequeueReusableCellWithReuseIdentifier:kPhotoCellIdentifier
                                                   forIndexPath:indexPath];
 
-    NSString *searchString     = self.searches[indexPath.section];
-    LXFlickrPhoto *flickrPhoto = self.searchResults[searchString][indexPath.row];
-    [cell configureWithFlickrPhoto:flickrPhoto];
+    [cell configureWithFlickrPhoto:[self p_flickrPhotoForIndexPath:indexPath]];
     
     return cell;
 }
@@ -131,17 +151,32 @@ static NSString * const kOFSampleAppAPISharedSecret = @"44dce4451bb55ea1";
 
 #pragma mark - UICollectionViewDelegate
 
-#pragma mark - UICollectionViewDelegateFlowLayout
-
-- (CGSize)collectionView:(UICollectionView *)collectionView
-                  layout:(UICollectionViewLayout *)collectionViewLayout
-  sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+- (void)collectionView:(UICollectionView *)collectionView
+    didSelectItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    NSString *searchString     = self.searches[indexPath.section];
-    LXFlickrPhoto *flickrPhoto = self.searchResults[searchString][indexPath.item];
-    return (CGSize){
-        flickrPhoto.thumbnailSize.width + kPadding, flickrPhoto.thumbnailSize.height + kPadding
-    };
+    [self performSegueWithIdentifier:@"ModalFlickrPhotoVC" sender:indexPath];
+}
+
+#pragma mark - 导航
+
+- (IBAction)unwindWithSegue:(UIStoryboardSegue *)segue { }
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)indexPath
+{
+    LXFlickrPhotoCell *cell =
+        (LXFlickrPhotoCell *)[self.collectionView cellForItemAtIndexPath:indexPath];
+
+    [segue.destinationViewController setImageWithURL:[self p_flickrPhotoForIndexPath:indexPath].largeImageURL
+                                    placeholderImage:cell.imageView.image];
+}
+
+#pragma mark - 辅助方法
+
+- (LXFlickrPhoto *)p_flickrPhotoForIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *searchString = self.searches[indexPath.section];
+    NSArray *searchResults = self.searchResults[searchString];
+    return searchResults[indexPath.item];
 }
 
 @end
